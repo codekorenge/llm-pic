bc58d3514b400241e8f16b58f3f90aabac753864
everhett-raman_psl <everhett_raman@persistent.com>
2024-02-24 16:54:55 +0800

    Integrated joy's tempates

--- openai_service.py
+++ openai_service.py
@@ -7,14 +7,71 @@ import pandas as pd
 from pandasai import Agent
 from pandasai.llm import AzureOpenAI
 
+
+# Requirements: two files, 2 question, one related to image, another related scalar.
+def make_query2(buffer1: bytes, buffer2: bytes, query: str):
+    llm = AzureOpenAI(
+        api_token="0c6bd275471947ffa99d5a0a9f536df6",
+        azure_endpoint="https://genai-lsr.openai.azure.com/",
+        api_version="2023-08-01-preview",
+        deployment_name="gpt-turbo-4k",
+        api_base="https://genai-lsr.openai.azure.com/",
+    )
+
+    dfs = []
+    if buffer1:
+        dfs.append(create_dataframe(buffer1))
+    if buffer2:
+        dfs.append(create_dataframe(buffer2))
+
+    # If array of df, put them inside [] with commas.
+    agent = Agent(dfs, config={"llm": llm}, memory_size=10)
+    response = agent.chat(query)
+    print(f"Response from OpenAI: <{response}>.")
+
+    return response
+
+
+# Requirements: two files, 2 question, one related to image, another related scalar.
+def make_query_only(query: str):
+    llm = AzureOpenAI(
+        api_token="0c6bd275471947ffa99d5a0a9f536df6",
+        azure_endpoint="https://genai-lsr.openai.azure.com/",
+        api_version="2023-08-01-preview",
+        deployment_name="gpt-turbo-4k",
+        api_base="https://genai-lsr.openai.azure.com/",
+    )
+    clinical = pd.read_csv("data/clinical_luad1.csv")
+    mutation = pd.read_csv("data/mutation_luad1.csv")
+
+    # If array of df, put them inside [] with commas.
+    agent = Agent([clinical, mutation], config={"llm": llm}, memory_size=10)
+    response = agent.chat(query)
+
+    print(f"Response from OpenAI: <{response}>.")
+
+    return response
+
+    # # Response either contains image or text only.
+    # if os.path.isfile(response):
+    #     file = open(response, mode="rb")
+    #     data = file.read()
+    #     base64_encoded_image = base64.b64encode(data).decode("utf-8")
+    #     print("Service returns image ...")
+    #     # NOTE: Delete image after reading but this can cause concurency issues.
+    #     # os.remove(response)
+    #     return base64_encoded_image, True
+    # else:
+    #     print("Service returns no-image ...")
+    #     return response, False
+
+
 # The purpose of this method is for uploading files and query regading the file data to OpenAI through PandasAI.
 # Therefore, at least one file and query must present.
-
-
-# two files, 2 question, one related to image, another related scalar.
+# Requirements: two files, 2 question, one related to image, another related scalar.
 def make_query(buffer: bytes, query: str):
     llm = AzureOpenAI(
-        # api_token=<insert-key>
+        api_token="0c6bd275471947ffa99d5a0a9f536df6",
         azure_endpoint="https://genai-lsr.openai.azure.com/",
         api_version="2023-08-01-preview",
         deployment_name="gpt-turbo-4k",
@@ -28,14 +85,6 @@ def make_query(buffer: bytes, query: str):
     response = agent.chat(query)
 
     print(response)
-
-    # try:
-    #     response2 = agent.chat("What is the total number of records in my data?.")
-    #     print("response 2 ..")
-    #     print(response2)
-    # except Exception as e:
-    #     print("exp-->")
-    #     print(e)
 
     # Response either contains image or text only.
     if os.path.isfile(response):
--- test_ai_service.py
+++ test_ai_service.py
@@ -1,22 +1,87 @@
 import sys
 import unittest
 
-from openai_service import make_query
+import openai_service
 
 # # TODO: Currently importing from outside directory. Need to make external package.
 # sys.path.append(".")
 # from openai_service import make_query
 
 
+# # Single file and related question works with graph.
+# class TestAIService(unittest.TestCase):
+#     def test_query_openai(self):
+
+#         file = open("data/clinical_luad1.csv", mode="rb")
+#         data = file.read()
+#         try:
+#             response, is_image = openai_service.make_query(
+#                 data, "Show the distribution of ALK?"
+#             )
+#             self.assertEqual(is_image, True)
+#             print(f"Response: <{response}>.")
+#         except Exception as e:
+#             print(e)
+
+
+# # Show the distribution of ALK
+# # When question is only related to single file, no graph returned but error response from openai.
+# class TestAIService(unittest.TestCase):
+#     def test_query_openai_for_two_files(self):
+#         file1 = open("data/mutation_luad1.csv", mode="rb")
+#         file2 = open("data/clinical_luad1.csv", mode="rb")
+#         data1 = file1.read()
+#         data2 = file2.read()
+#         try:
+#             response, is_image = openai_service.make_query2(
+#                 data1, data2, "Show the distribution of ALK"
+#             )
+
+#             self.assertEqual(is_image, True)
+#             print(f"Response: <{response}>.")
+
+#         except Exception as e:
+#             print(e)
+
+
+# # Compare the distribution of values of EGFR for each values from primary_therapy_outcome_success
+# # Question is realted to both files.
+# class TestAIService(unittest.TestCase):
+#     def test_query_openai_for_two_files(self):
+#         file1 = open("data/mutation_luad1.csv", mode="rb")
+#         file2 = open("data/clinical_luad1.csv", mode="rb")
+#         data1 = file1.read()
+#         data2 = file2.read()
+#         try:
+#             response, is_image = openai_service.make_query2(
+#                 data1,
+#                 data2,
+#                 "Compare the distribution of values of EGFR for each values from primary_therapy_outcome_success",
+#             )
+
+#             self.assertEqual(is_image, True)
+#             print(f"Response: <{response}>.")
+
+#         except Exception as e:
+#             print(e)
+
+
+# Compare the distribution of values of EGFR for each values from primary_therapy_outcome_success
+# Question is realted to both files.
 class TestAIService(unittest.TestCase):
-    def test_query_openai(self):
-
-        file = open("data/clinical_luad1.csv", mode="rb")
-        data = file.read()
+    def test_query_openai_question_only(self):
         try:
-            response, is_image = make_query(data, "Show the distribution of ALK?")
+            response = openai_service.make_query_only(
+                "Compare the distribution of values of EGFR for each values from primary_therapy_outcome_success",
+            )
+            print(f"Response1: <{response}>.")
 
-            self.assertEqual(is_image, True)
+            response = openai_service.make_query_only(
+                "Count the total number of effect of EGFR.",
+            )
+            # self.assertEqual(is_image, True)
+            print(f"Response2: <{response}>.")
+
         except Exception as e:
             print(e)
 
